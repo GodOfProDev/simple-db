0cd15a4baf91e951b1b64d9fe8d12fc761f78141
GodOfPro <1387ilia@gmail.com>
2023-09-22 15:49:27 +0330

    Implement delete user

--- internal/handlers/handler_deleteuser.go
+++ internal/handlers/handler_deleteuser.go
@@ -1,7 +1,22 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
 
 func (h Handlers) HandleDeleteUser(c *fiber.Ctx) error {
-	return c.SendString("delete user")
+	id := c.Params("id")
+
+	uid, err := uuid.Parse(id)
+	if err != nil {
+		return err
+	}
+
+	err = h.store.DeleteUser(uid)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).SendString("Successfully deleted the account")
 }
